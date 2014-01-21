3aec4d3c67f0f3513088eb67b4f7c63d2a44d367
sagesmith <sagesmith@gmail.com>
2014-01-21 17:33:16 -0500

    first commit

---
+++ estreet_client/default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="estreet_client._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        URI<asp:RadioButtonList ID="rblURIs" runat="server">
+            <asp:ListItem Selected="True">https://electionstreet.com</asp:ListItem>
+            <asp:ListItem>http://estreet.boe.nyc.ny.us</asp:ListItem>
+            <asp:ListItem>http://future.boe.nyc.ny.us</asp:ListItem>
+            <asp:ListItem>http://city.boe.nyc.ny.us</asp:ListItem>
+            <asp:ListItem>http://state.boe.nyc.ny.us</asp:ListItem>
+            <asp:ListItem>http://congress.boe.nyc.ny.us</asp:ListItem>
+            <asp:ListItem>http://lastdecade.boe.nyc.ny.us</asp:ListItem>
+        </asp:RadioButtonList>
+        Referer<br />
+        <asp:TextBox ID="txtReferer" runat="server"></asp:TextBox>
+        <br />
+        County<br />
+        <asp:DropDownList ID="ddlCounty" runat="server">
+            <asp:ListItem Selected="True">New York</asp:ListItem>
+            <asp:ListItem>Bronx</asp:ListItem>
+            <asp:ListItem>Kings</asp:ListItem>
+            <asp:ListItem>Queens</asp:ListItem>
+            <asp:ListItem>Richmond</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        Street Number<br />
+        <asp:TextBox ID="txtStreetNumber" runat="server"></asp:TextBox>
+        <br />
+        Street<br />
+        <asp:TextBox ID="txtStreet" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Lookup" />
+        <br />
+        <br />
+        <br />
+        <asp:Panel ID="pnlResults" runat="server">
+        </asp:Panel>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ estreet_client/default.aspx.cs
@@ -0,0 +1,127 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace estreet_client
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            pnlResults.Controls.Clear();
+
+            var client = new RestClient(rblURIs.SelectedValue);
+            var request = new RestRequest("api/nyc/addresses", Method.GET);
+
+            request.AddHeader("referer", txtReferer.Text);
+            
+            request.AddParameter("county", ddlCounty.SelectedValue);
+            request.AddParameter("streetnumber", txtStreetNumber.Text);
+            request.AddParameter("streetname", txtStreet.Text);
+
+            RestResponse<estreet> response = (RestResponse<estreet>)client.Execute<estreet>(request);
+            
+            if(response.Data==null)
+            {
+                Label error = new Label();
+                error.Text = "There is an error with the estreet call.  Double check the Referer value. [Error: " + response.StatusDescription + "]";
+                pnlResults.Controls.Add(error);
+                return;
+
+            }
+
+            TreeView tvResults = new TreeView();
+            
+            TreeNode ss = new TreeNode("search status");
+            ss.ChildNodes.Add(new TreeNode("contains warning: " + response.Data.searchStatus.containsWarning.ToString()));
+            ss.ChildNodes.Add(new TreeNode("error: " + response.Data.searchStatus.error));
+            ss.ChildNodes.Add(new TreeNode("return code: " + response.Data.searchStatus.returnCode));
+            if (response.Data.searchStatus.streetSuggestions != null)
+            {
+                TreeNode s = new TreeNode("street suggestions");
+                foreach (string ssug in response.Data.searchStatus.streetSuggestions)
+                    s.ChildNodes.Add(new TreeNode(ssug));
+                ss.ChildNodes.Add(s);
+            }
+            ss.ChildNodes.Add(new TreeNode("valid address: " + response.Data.searchStatus.validAddress.ToString()));
+            ss.ChildNodes.Add(new TreeNode("warning: " + response.Data.searchStatus.warning));
+            tvResults.Nodes.Add(ss);
+            
+            if(response.Data.standardizedAddress!=null)
+            { 
+                TreeNode sa = new TreeNode("standardized address");
+                sa.ChildNodes.Add(new TreeNode("BoE preferred sort street name: " + response.Data.standardizedAddress.BoEPreferredSortStreetName));
+                sa.ChildNodes.Add(new TreeNode("BoE preferred street name: " + response.Data.standardizedAddress.BoEPreferredStreetName));
+                sa.ChildNodes.Add(new TreeNode("city: " + response.Data.standardizedAddress.city));
+                sa.ChildNodes.Add(new TreeNode("postal code: " + response.Data.standardizedAddress.postalCode));
+                sa.ChildNodes.Add(new TreeNode("state: " + response.Data.standardizedAddress.stateProvTerr));
+                sa.ChildNodes.Add(new TreeNode("street name: " + response.Data.standardizedAddress.streetName));
+                sa.ChildNodes.Add(new TreeNode("street number: " + response.Data.standardizedAddress.streetNumber));
+                tvResults.Nodes.Add(sa);
+            }
+
+            if(response.Data.politicalLayer!=null)
+            { 
+                TreeNode pl = new TreeNode("political layer");
+                pl.ChildNodes.Add(new TreeNode("assembly district: " + response.Data.politicalLayer.assemblyDistrict));
+                pl.ChildNodes.Add(new TreeNode("city council district: " + response.Data.politicalLayer.cityCouncilDistrict));
+                pl.ChildNodes.Add(new TreeNode("election district: " + response.Data.politicalLayer.electionDistrict));
+                pl.ChildNodes.Add(new TreeNode("is split ed: " + response.Data.politicalLayer.isSplitED.ToString()));
+                pl.ChildNodes.Add(new TreeNode("municiple court district: " + response.Data.politicalLayer.municipalCourtDistrict));
+                pl.ChildNodes.Add(new TreeNode("state senate district: " + response.Data.politicalLayer.stateSenateDistrict));
+                tvResults.Nodes.Add(pl);
+            }
+
+            tvResults.ExpandAll();
+            pnlResults.Controls.Add(tvResults);
+
+        }
+    }
+    public class estreet
+    {
+        public searchStatus searchStatus { get; set; }
+        public standardizedAddress standardizedAddress { get; set; }
+        public politicalLayer politicalLayer { get; set; }
+    }
+    public class searchStatus
+    {
+        public bool containsWarning { get; set; }
+        public string error { get; set; }
+        public string returnCode { get; set; }
+        public List<string> streetSuggestions { get; set; }
+        public bool validAddress { get; set; }
+        public string warning { get; set; }
+    }
+    public class standardizedAddress
+    {
+        public string BoEPreferredSortStreetName { get; set; }
+        public string BoEPreferredStreetName { get; set; }
+        public string city { get; set; }
+        public string postalCode { get; set; }
+        public string stateProvTerr { get; set; }
+        public string streetName { get; set; }
+        public string streetNumber { get; set; }
+    }
+    public class politicalLayer
+    {
+        public string assemblyDistrict { get; set; }
+        public string cityCouncilDistrict { get; set; }
+        public string congressionalDistrict { get; set; }
+        public string electionDistrict { get; set; }
+        public bool isSplitED { get; set; }
+        public string municipalCourtDistrict { get; set; }
+        public string stateSenateDistrict { get; set; }
+    }
+}
