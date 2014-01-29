50e0e1dda43a63a1bc1823095038471105a05edc
sagesmith <sagesmith@gmail.com>
2014-01-29 15:59:57 -0500

    target framework 3.5

--- estreet_client/default.aspx.cs
+++ estreet_client/default.aspx.cs
@@ -1,12 +1,7 @@
 ﻿using RestSharp;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 
 namespace estreet_client
 {
